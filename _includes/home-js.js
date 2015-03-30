a30baef259f68c2bc47411c6b91cac6a79553850
Martin Steel <martin@martinsteel.co.uk>
2015-03-30 21:10:24 +0100

    Fix mobile nav

--- _includes/home-js.js
+++ _includes/home-js.js
@@ -7,8 +7,10 @@
 
       var title = document.getElementById('home-title');
       var bodyTop = (document.documentElement.scrollTop?document.documentElement:document.body).getBoundingClientRect().top;
-      var headingTop = title.getBoundingClientRect().top;
-      var headingPos = headingTop - bodyTop;
+      var headingPos = title.getBoundingClientRect().top - bodyTop;
+      var hero = document.getElementsByClassName('home-masthead')[0];
+      var heroPos = hero.getBoundingClientRect().bottom - bodyTop;
+      var masthead = document.getElementsByClassName('masthead')[0];
      
       setInterval(function() {
         if ( didScroll ) {
@@ -16,6 +18,14 @@
           var scrollPos = (document.documentElement.scrollTop?document.documentElement:document.body).scrollTop;
           var opacity = (scrollPos < headingPos) ? (headingPos - scrollPos) / headingPos : 0;
           title.style.opacity = opacity;
+
+          if(scrollPos > heroPos) { 
+            masthead.className = masthead.className.replace( ' masthead-home', '' );
+            console.log('greater');
+          } else {
+            masthead.classList ? masthead.classList.add('masthead-home') : masthead.className += ' masthead-home';  
+            console.log('smaller');          
+          }
         } 
       }, 250);
     }
